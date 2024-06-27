5b49f15e3180f40acc1e7a69ae05dbdd01b900bc
Shashank1618 <shashankawasthi426@gmail.com>
2024-06-27 14:17:55 +0530

    Built in function

---
+++ String.js
@@ -0,0 +1,22 @@
+let str="Robin"
+
+console.log(str.length)   //length of a string
+//5
+
+console.log(str.indexOf("o"))  //Search for the targeted string or character  
+// 1
+
+console.log(str.slice(0,4)) //print the character within these index [Start, End)
+//Robi
+
+console.log(str.substr(1,3)) //print 3 character from the index 1 of the given string 
+//obi
+
+console.log(str.replace(str,"Jessica"))  //Replace the given string
+//Jessica
+
+let str1="Hi my name is Traio"
+const word=str1.split("")       //convert the string into an array using a delimeter
+console.log(word)               //variable_name.split("delimeter")
+                                                                                                                                                                                                                                                       
+
